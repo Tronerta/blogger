885dbe11cc6217d4c77ea212bed472b478e07697
tronerta <tronerta.web@gmail.com>
2019-05-04 16:39:39 +0200

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
 	root to: 'articles#index'
   resources :articles do
   	resources :comments
@@ -7,6 +6,6 @@ Rails.application.routes.draw do
   resources :tags
   resources :authors
   resources :author_sessions, only: [ :new, :create, :destroy ]
-	get 'login'  => 'author_sessions#new'
-	get 'logout' => 'author_sessions#destroy'
+	get 'login', to: 'author_sessions#new'
+	get 'logout',to: 'author_sessions#destroy'
 end
