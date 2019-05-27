9bd91e201b833857eddedf864683577fe5335ecf
tronerta <tronerta.web@gmail.com>
2019-05-27 18:17:53 +0300

    Fixed comments validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
 
-  validates_length_of :name, maximum: 30
+  validates_length_of :author_name, maximum: 30
   validates_length_of :body, maximum: 256
 end
